5194920878aab4b28887335dd2225d1d83d33f03
Alex Acer <you@example.com>
2024-11-06 16:15:37 +0500

    add flake

--- flake.nix
+++ flake.nix
@@ -7,12 +7,12 @@
 
   outputs = { self, nixpkgs, ... }: let
     system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
   in {
-    devShells."${system}".default = let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in pkgs.mkShell {
+    devShells."${system}" = {
+      default = pkgs.mkShell {
       packages = with pkgs; [
         git
         nodejs_20
@@ -40,6 +40,12 @@
         # npm config set registry http://registry.npmjs.org/ 
         # npm config set strict-ssl=false
 
+        # Optional: Auto-cd into webapp directory
+        if [ -d "packages/webapp" ]; then
+          cd packages/webapp
+          echo "Changed directory to packages/webapp"
+        fi
+
         # Initialize project
         if [ ! -f "pnpm-lock.yaml" ]; then
           echo "Installing pnpm and project dependencies..."
@@ -48,15 +54,55 @@
           pnpm install
         fi
 
-        # Optional: Auto-cd into webapp directory
-        if [ -d "packages/webapp" ]; then
-          cd packages/webapp
-          echo "Changed directory to packages/webapp"
-        fi
-
         # You can uncomment this if you want the dev server to start automatically
         # npm run dev
       '';
+    };
+
+    node = pkgs.mkShell {
+      packages = with pkgs; [
+        git
+        nodejs_20
+        pnpm_8
+        # nodePackages.pnpm
+        #(yarn.override { nodejs = nodejs_18; })
+        # Add nvm if needed
+        # nvm
+      ];
+
+      # Environment variables
+      shellHook = ''
+        echo "node `${pkgs.nodejs}/bin/node --version`"
+        echo "update"
+
+        # Create app directory and set working directory
+              mkdir -p app
+              cd app
+
+              # Copy package.json files
+              for file in package*.json; do
+                if [ -f "$file" ]; then
+                  cp "$file" .
+                fi
+              done
+
+              # Create and copy package.json files for each package
+              mkdir -p packages/{eslint-config,eslint-rules,extension,prettier-config,shared,webapp}
+              
+              for dir in eslint-config eslint-rules extension prettier-config shared webapp; do
+                if [ -f "packages/$dir/package.json" ]; then
+                  cp "packages/$dir/package"*.json "./packages/$dir/"
+                fi
+              done
+
+              # Copy patches directory if it exists
+              if [ -d "patches" ]; then
+                cp -r patches /opt/app/
+              fi
+
+              echo "Development environment setup complete"
+      '';
+    };    
     };
   };
 }
